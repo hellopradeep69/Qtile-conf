0acfe6752eac086db38d5a25d694ac39505a0b60
k.pradeep <hellopradeep69@gmail.com>
2025-10-24 22:04:05 +0530

    initial commit

---
+++ autostart.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Clipboard manager
+wl-paste --watch cliphist store &
+
+#copyq to have clipboard
+copyq &
+
+# Notification daemon
+dunst &
+
+# Start compositor
+picom &
+
+# feh --bg-scale ~/Pictures/Wallpapers/skeletonparty.jpg &
+
+# Custom scripts
+~/.local/bin/keybind.sh &
+~/.local/bin/battery-notify2.sh &
+
+# ~/.config/polybar/launch.sh --colorblocks &
+# ~/.config/polybar/launch.sh --shades &
